d643bee62a7f1bb1e6152dda185a7e31f2f805f1
Jacob Bahr <47398768+Jbahr2@users.noreply.github.com>
2022-05-06 08:01:55 -0600

    Fixed mirror code

--- CameraRigDemo/Assets/MoveCamera.cs
+++ CameraRigDemo/Assets/MoveCamera.cs
@@ -9,26 +9,23 @@ public class MoveCamera : MonoBehaviour {
     private GameObject playerObj = null;
     private GameObject house1 = null;
     private GameObject house2 = null;
-    private int mCurrentIndex = 0, dist;
-    public Vector3 pos = 
+    public Vector3 pos = new Vector3(0.0f,0.0f,-9.460979f);
     public float Speed = 2.0f;
 	void Start()
     {
 
         if (playerObj == null)
-            playerObj = GameObject.Find("Player");
+            playerObj = GameObject.Find("VRCamera");
         if (house1 == null)
-            house1 = GameObjecct.Find("House fix");
+            house1 = GameObject.Find("House fix");
         if (house2 == null)
-            house2 = GameObjecct.Find("House-9-2");
-        dist = house1.transform.position.z - house2.transform.position.z;
+            house2 = GameObject.Find("House-9-2");
     }
 	// Update is called once per frame
 	void Update () {
 
-        Vector3 currentPos = Positions[mCurrentIndex];
-
+        Vector3 currentPos = playerObj.transform.position + new Vector3(0.0f,0.0f,0.0f);
         transform.position = Vector3.Lerp(transform.position, currentPos, Speed * Time.deltaTime);
-
+        transform.rotation = playerObj.transform.rotation;
     }
 }
